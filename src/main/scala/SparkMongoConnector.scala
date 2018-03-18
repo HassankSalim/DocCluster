6aa47516c37edd62257bb85a1c74db21026d4744
Hassan K Salim <hassanksalim@gmail.com>
2018-03-18 11:42:09 +0530

    Add Singleton class for Spark Mongodb Connector and Remove TestScripts

---
+++ src/main/scala/SparkMongoConnector.scala
@@ -0,0 +1,37 @@
+import com.mongodb.spark._
+import com.mongodb.spark.config.ReadConfig
+import com.mongodb.spark.rdd.MongoRDD
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.{SparkConf, SparkContext}
+import org.bson.Document
+
+
+object SparkMongoConnector {
+
+  var sc : SparkContext = _
+  var sparkSess : SparkSession = _
+  var rdd : MongoRDD[Document] = _
+
+  def apply(appName: String, inUrl: String, outUrl : String = "") = {
+    val conf = new SparkConf()
+    conf.setMaster("local")
+    conf.setAppName(appName)
+    sc = new SparkContext(conf)
+
+    val spark = SparkSession.builder()
+      .master("local")
+      .appName("MongoSparkConnectorIntro")
+      .config("spark.mongodb.input.uri", inUrl)
+      .config("spark.mongodb.output.uri", outUrl)
+      .getOrCreate()
+
+    rdd = MongoSpark.load(sc)
+
+  }
+
+  def getCount() = rdd.count()
+
+  def getRdd = rdd
+
+  def filter(customFilter : (Document) => Boolean, mongoRDD: MongoRDD[Document]) = { mongoRDD.filter(doc => customFilter(doc)) }
+}
