f25b61cced004337ec4f07949f9cfce298917da9
hasali <ext-hassan.salim@medlife.com>
2018-05-27 23:50:24 +0530

    Add mongoUrlConstructor to work with application.conf

--- src/main/scala/SparkMongoConnector.scala
+++ src/main/scala/SparkMongoConnector.scala
@@ -1,5 +1,6 @@
 import com.mongodb.spark._
 import com.mongodb.spark.rdd.MongoRDD
+import com.typesafe.config.ConfigFactory
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.{SparkConf, SparkContext}
 import org.bson.Document
@@ -9,14 +10,22 @@ object SparkMongoConnector {
 
   var sparkSess : SparkSession = _
   var rdd : MongoRDD[Document] = _
+  val conf = ConfigFactory.load();
+
+  val username = conf.getString("appConfig.mongo.user");
+  val password = conf.getString("appConfig.mongo.password");
+
+  def mongoUrlConstructor(url: String): String = {
+    return s"mongodb://$username:$password@$url"
+  }
 
   def apply(appName: String, inUrl: String, outUrl : String = "") = {
 
     val spark = SparkSession.builder()
       .master("local")
       .appName(appName)
-      .config("spark.mongodb.input.uri", inUrl)
-      .config("spark.mongodb.output.uri", outUrl)
+      .config("spark.mongodb.input.uri", mongoUrlConstructor(inUrl))
+      .config("spark.mongodb.output.uri", mongoUrlConstructor(outUrl))
       .getOrCreate()
 
   }
